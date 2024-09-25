254b5af694970f4ffb7524bf8d6e40a67852ab3e
Stefano Esposito <stefano.esposito@abstract.it>
2024-09-25 11:18:18 +0200

    removed commented code

--- src/i18n.js
+++ src/i18n.js
@@ -45,26 +45,3 @@ i18n
     })
 
 export default i18n
-
-
-// i18n.js
-// import i18n from 'i18next';
-// import { initReactI18next } from 'react-i18next';
-// import LanguageDetector from 'i18next-browser-languagedetector';
-// import HttpApi from 'i18next-http-backend';
-//
-// i18n
-//     .use(HttpApi)
-//     .use(LanguageDetector)
-//     .use(initReactI18next)
-//     .init({
-//         fallbackLng: 'en',
-//         interpolation: {
-//             escapeValue: false,
-//         },
-//         backend: {
-//             loadPath: '/locales/{{lng}}/{{ns}}.json',
-//         },
-//     });
-//
-// export default i18n;
